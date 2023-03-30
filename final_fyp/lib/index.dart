fba5a7a8ad4a2441af3081dfc2b69412b843d2a7
alihusain <77673121+ronit450@users.noreply.github.com>
2023-03-30 15:48:07 +0500

    All files completed

--- final_fyp/lib/index.dart
+++ final_fyp/lib/index.dart
@@ -1,34 +1,108 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: import_of_legacy_library_into_null_safe
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+
+const kGoogleApiKey = 'AIzaSyAT8pCVh8o7Q9IoYxBRJ7WJ3ndmw1NZCAk';
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController _mapController ;
+  final TextEditingController _searchController = TextEditingController();
+  LatLng _center = const LatLng(45.521563, -122.677433);
+  List<PlacesSearchResult> _placesList = [];
 
-  // This widget is the root of your application.
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  Future<void> _searchPlace(String query) async {
+    final location = Location(_center.latitude, _center.longitude);
+    final result = await _places.searchByText(query, location: location);
+
+    if (result.status == 'OK') {
+      setState(() {
+        _placesList = result.results;
+      });
+    } else {
+      print('Error: ${result.errorMessage}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              hintText: 'Search',
+              border: InputBorder.none,
+              contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+            ),
+            onSubmitted: (value) {
+              _searchPlace(value);
+            },
+          ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+                markers: Set<Marker>.of(_placesList.map((place) {
+                  final marker = Marker(
+                    markerId: MarkerId(place.id),
+                    position: LatLng(
+                      place.geometry.location.lat,
+                      place.geometry.location.lng,
+                    ),
+                    infoWindow: InfoWindow(
+                      title: place.name,
+                      snippet: place.formattedAddress,
+                    ),
+                  );
+                  _mapController.showMarkerInfoWindow(marker.markerId);
+                  return marker;
+                })),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _placesList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final place = _placesList[index];
+                  return ListTile(
+                    title: Text(place.name),
+                    subtitle: Text(place.formattedAddress),
+                    onTap: () {
+                      final LatLng position = LatLng(
+                        place.geometry.location.lat,
+                        place.geometry.location.lng,
+                      );
+                      _mapController
+                          .animateCamera(CameraUpdate.newLatLng(position));
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
-
-      debugShowCheckedModeBanner: false,
-      debugShowMaterialGrid: false,
-      initialRoute: '/',
-        routes: {
-          '/': (context) => Welcome(),
-          // '/login': (context) => LoginScreen(),
-          // '/home': (context) => Homepage(),
-          // '/signup': (context) => RegistrationScreen(),
-          // '/ride_request': (context) => ride_request(
-          //     ModalRoute.of(context)!.settings.arguments as Position),
-          // '/request_page': (context) => RideRequest(),
-          // '/main_screen': (context) => Main_Screen(),
     );
   }
 }
-
--- final_fyp/lib/welcome.dart
+++ final_fyp/lib/welcome.dart
@@ -1,344 +1,138 @@
-// ignore_for_file: prefer_final_fields, non_constant_identifier_names, unused_element, prefer_const_literals_to_create_immutables, unnecessary_null_comparison
+// // ignore_for_file: unnecessary_import
+// import 'package:google_maps_flutter/google_maps_flutter.dart'
+//     show LatLng, BitmapDescriptor, Marker, Location;
+// import 'package:google_maps_webservice/places.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// // ignore: import_of_legacy_library_into_null_safe
+// import 'package:google_maps_webservice/places.dart';
+// import 'dart:async';
+// import 'package:geocoding/geocoding.dart';
+// import 'package:geolocator/geolocator.dart';
 
-import 'dart:async';
+// class Location {
+//   final double latitude;
+//   final double longitude;
+
+//   Location(this.latitude, this.longitude);
+// }
+
+// class MapPage extends StatefulWidget {
+//   @override
+//   _MapPageState createState() => _MapPageState();
+// }
+
+// class _MapPageState extends State<MapPage> {
+//   Completer<GoogleMapController> _controller = Completer();
+//   TextEditingController _searchController = TextEditingController();
+//   LatLng _center = LatLng(37.7749, -122.4194);
+//   final _places = GoogleMapsPlaces(apiKey: 'AIzaSyAT8pCVh8o7Q9IoYxBRJ7WJ3ndmw1NZCAk');
+//   List<PlacesSearchResult> _placesList = [];
+
+//   Set<Marker> _markers = {};
+
+//   @override
 
 
-import 'package:final_fyp/Reverse_Geocoding/place_searcher_methods.dart';
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: TextField(
+//           controller: _searchController,
+//           decoration: InputDecoration(
+//             hintText: 'Search for a place',
+//             border: InputBorder.none,
+//             suffixIcon: IconButton(
+//               icon: Icon(Icons.search),
+//               onPressed: () {
+//                 _searchPlace(_searchController.text);
+//               },
+//             ),
+//           ),
+//         ),
+//       ),
+//       body: Stack(
+//         children: [
+//           GoogleMap(
+//             initialCameraPosition: CameraPosition(
+//               target: _center,
+//               zoom: 12.0,
+//             ),
+//             onMapCreated: (GoogleMapController controller) {
+//               _controller.complete(controller);
+//             },
+//           ),
+//           Positioned(
+//             bottom: 0,
+//             left: 0,
+//             right: 0,
+//             child: Container(
+//               height: MediaQuery.of(context).size.height * 0.3,
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 borderRadius: BorderRadius.vertical(
+//                   top: Radius.circular(20),
+//                 ),
+//               ),
+//               child: ListView.builder(
+//                 itemCount: _placesList.length,
+//                 itemBuilder: (context, index) {
+//                   final place = _placesList[index];
+//                   return ListTile(
+//                     title: Text(place.name),
+//                     subtitle: Text(place.formattedAddress),
+//                     onTap: () {
+//                       _moveCameraToLocation(
+//                         place.geometry.location.lat,
+//                         place.geometry.location.lng,
+//                       );
+//                     },
+//                   );
+//                 },
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-import 'package:final_fyp/dataHandler/dataHandler.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:final_fyp/utils/routes.dart';
-import 'package:final_fyp/google_fonts.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
+// Future<void> _searchPlace(String query) async {
+//     final location1 = Location(_center.latitude, _center.longitude);
+//     final result = await _places.searchByText(query, location: location1);
 
-class Main_Screen extends StatefulWidget {
-  const Main_Screen({Key? key}) : super(key: key);
+//     if (result.status == 'OK') {
+//       setState(() {
+//         _placesList = result.results;
+//         _markers.clear();
+//         for (var place in _placesList) {
+//           _markers.add(
+//             Marker(
+//               markerId: MarkerId(place.name),
+//               position: LatLng(
+//                 place.geometry!.location.lat,
+//                 place.geometry!.location.lng,
+//               ),
+//               infoWindow: InfoWindow(
+//                 title: place.name,
+//                 snippet: place.formattedAddress,
+//               ),
+//             ),
+//           );
+//         }
+//       });
+//     } else {
+//       print('Error: ${result.errorMessage}');
+//     }
+//   }
 
-  @override
-  State<Main_Screen> createState() => _Main_ScreenState();
-}
 
-class _Main_ScreenState extends State<Main_Screen> {
-  User? user = FirebaseAuth.instance.currentUser;
-  final _auth = FirebaseAuth.instance;
-  UserModel loggedInUser = UserModel();
-  void initState() {
-    super.initState();
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(user!.uid)
-        .get()
-        .then((value) {
-      this.loggedInUser = UserModel.fromMap(value.data());
-      setState(() {});
-    });
-  }
 
-  // Declaration of variables.
-  double bottom_padding_of_map = 0.0;
-  String string_selected_date = "";
-  String home_temp = "Add Home";
-  DateTime selectedDate = DateTime.now();
-  TimeOfDay selectedTime = TimeOfDay.now();
-  TextEditingController _dateController = TextEditingController();
-  TextEditingController _timeController = TextEditingController();
-  Completer<GoogleMapController> _google_map_controller = Completer();
-  late GoogleMapController new_google_map_controller_for_saving;
-
-  late Position current_position;
-  var geoLocater = Geolocator();
-
-  void locateCurrentpos() async {
-    LocationPermission permission;
-    permission = await Geolocator.requestPermission();
-
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    current_position = position;
-    // As the location comes with Geopoint with lattitude and longitude
-    LatLng lat_position = LatLng(position.latitude, position.longitude);
-
-    CameraPosition cameraPosition =
-        new CameraPosition(target: lat_position, zoom: 14);
-    new_google_map_controller_for_saving
-        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-
-    String address =
-        await placeSearcherMethods.searchCoodinateAddress(position, context);
-    print("ap yhan rehte hen?" + address);
-  }
-
-  Position get_current_position() {
-    return current_position;
-  }
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    // Select time field
-    Future<void> _selectTime(BuildContext context) async {
-      final TimeOfDay? picked_s = await showTimePicker(
-        context: context,
-        initialTime: selectedTime,
-      );
-      if (picked_s != null)
-        // ignore: curly_braces_in_flow_control_structures
-        setState(() {
-          selectedTime = picked_s;
-          DateTime parsedTime =
-              DateFormat.jm().parse(picked_s.format(context).toString());
-          String formattedTime = DateFormat('HH:mm:ss').format(parsedTime);
-          _timeController.text = formattedTime;
-        });
-    }
-
-    Widget pickup_and_dropoff(String text) {
-      return Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(5.0),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black54,
-              blurRadius: 6.0,
-              offset: Offset(0.7, 0.7),
-              spreadRadius: 0.5,
-            )
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            children: [
-              Icon(
-                CupertinoIcons.search,
-                color: Colors.purple,
-              ),
-              Text(text),
-              SizedBox(
-                height: 26.0,
-              ),
-              Divider_Widget(),
-            ],
-          ),
-        ),
-      );
-    }
-
-    // Select Date field for carpool request
-
-    Future<void> _selectDate(BuildContext context) async {
-      final DateTime? picked = await showDatePicker(
-          context: context,
-          initialDate: selectedDate,
-          firstDate: DateTime.now(),
-          lastDate: DateTime(2101));
-      if (picked != null && picked != selectedDate) {
-        setState(() {
-          DateFormat dateFormat = DateFormat("yyyy-MM-dd");
-          string_selected_date = dateFormat.format(selectedDate);
-          var date =
-              "${picked.toLocal().day}/${picked.toLocal().month}/${picked.toLocal().year}";
-          selectedDate = picked;
-          _dateController.text = date;
-        });
-      }
-    }
-
-    Widget home_and_work_row(
-        String message1, String message2, Icon icon_to_put) {
-      return Row(
-        children: [
-          Icon(icon_to_put.icon),
-          SizedBox(
-            width: 12.0,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                message1,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(fontSize: 14),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                message2,
-                style: TextStyle(color: Colors.black54),
-              )
-            ],
-          )
-        ],
-      );
-    }
-
-    Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      extendBody: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.red),
-          onPressed: () {
-            // passing this to our root
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      
-      body: Stack(
-        children: [
-          GoogleMap(
-            padding: EdgeInsets.only(bottom: bottom_padding_of_map),
-            compassEnabled: true,
-            mapType: MapType.normal,
-            myLocationEnabled: true,
-            zoomGesturesEnabled: true,
-            zoomControlsEnabled: true,
-            myLocationButtonEnabled: true,
-            initialCameraPosition: _kGooglePlex,
-
-            // Map being created
-            onMapCreated: (GoogleMapController controller) {
-              _google_map_controller.complete(controller);
-              new_google_map_controller_for_saving = controller;
-              setState(() {
-                bottom_padding_of_map = 300.0;
-              });
-
-              locateCurrentpos();
-            },
-          ),
-          Positioned(
-            left: 0.0,
-            right: 0.0,
-            bottom: 0.0,
-            child: Container(
-              height: 320.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15.0),
-                    topRight: Radius.circular(15.0)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    blurRadius: 14.0,
-                    offset: Offset(0.7, 0.7),
-                    spreadRadius: 0.5,
-                  )
-                ],
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 24.0, vertical: 18),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 5.0),
-                      Text(
-                        " Hi ${loggedInUser.firstName} ${loggedInUser.secondName}",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        "Where to?",
-                        style:
-                            TextStyle(fontSize: 19, fontFamily: 'Brand-Bold'),
-                      ),
-                      SizedBox(
-                        height: 26.0,
-                      ),
-// As this entire container is the search dropoff field so now we have to wrap it with gesturable
-                      GestureDetector(
-                        onTap: () {
-                          // Now here we will make another page where user will be redirected.
-                          Navigator.pushNamed(context, Myroutes.ride_request,
-                              arguments: current_position);
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(5.0),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black,
-                                blurRadius: 6.0,
-                                offset: Offset(0.7, 0.7),
-                                spreadRadius: 0.5,
-                              )
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Row(children: [
-                              Icon(
-                                Icons.search,
-                                color: Colors.purple,
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text("Search Drop Off")
-                            ]),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 26.0),
-
-                      home_and_work_row(
-                          (Provider.of<carpool_data>(context).pickupLocation !=
-                                  null
-                              ? Provider.of<carpool_data>(context)
-                                  .pickupLocation
-                                  ?.placeName
-                              : "Add home")!,
-                          "Your living address",
-                          Icon(Icons.home)),
-                      SizedBox(height: 10),
-                      Divider_Widget(),
-                      SizedBox(height: 20.0),
-                      home_and_work_row("Add University ",
-                          "Your University address", Icon(Icons.work)),
-                    ]),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AlwaysDisabledFocusNode extends FocusNode {
-  @override
-  bool get hasFocus => false;
-}
-
-class GeolocatorService {
-  Future<Position?> determinePosition() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        return Future.error('Location Not Available');
-      }
-    } else {
-      throw Exception('Error');
-    }
-    return await Geolocator.getCurrentPosition();
-  }
-}
+//   Future<void> _moveCameraToLocation(double latitude, double longitude) async {
+//     final controller = await _controller.future;
+//     controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+//       target: LatLng(latitude, longitude),
+//       zoom: 15.0,
+//     )));
+//   }
+// }
